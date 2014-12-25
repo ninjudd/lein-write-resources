ff3c8c0a2a6085d496198bbe65b734a5e15485e4
Justin Balthrop <git@justinbalthrop.com>
2014-12-25 16:22:37 -0700

    description and url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject lein-write-resources "0.1.0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A very simple Leiningen plugin to write resources on classpath to files."
+  :url "http://github.com/ninjudd/lein-write-resources"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[leinjacker "0.4.0"]]
